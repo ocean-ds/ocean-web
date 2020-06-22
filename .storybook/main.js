b8e1ecb12044bf9bbea108a60e5322c0841f5355
Elton Garbin <eltongarbin@gmail.com>
2020-06-22 16:18:26 -0300

    replaces babel by typescript in the storybook configs

--- .storybook/main.js
+++ .storybook/main.js
@@ -13,10 +13,10 @@ module.exports = {
 
     config.module.rules.push({
       test: /\.(ts|tsx)$/,
-      loader: require.resolve('babel-loader'),
-      options: {
-        presets: [['react-app', { flow: false, typescript: true }]],
-      },
+      use: [
+        { loader: require.resolve('ts-loader') },
+        { loader: require.resolve('react-docgen-typescript-loader') },
+      ],
     });
     config.resolve.extensions.push('.ts', '.tsx');
 
