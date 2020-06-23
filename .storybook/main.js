2990dbe4d9aa1b1ab30a68624b8339fd95228ded
Elton Garbin <eltongarbin@gmail.com>
2020-06-23 10:52:22 -0300

    covering with tests

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,8 +2,7 @@ const path = require('path');
 
 module.exports = {
   stories: ['../src/**/*.stories.tsx'],
-  // Add any Storybook addons you want here: https://storybook.js.org/addons/
-  addons: [],
+  addons: ['@storybook/addon-viewport/register'],
   webpackFinal: async (config) => {
     config.module.rules.push({
       test: /\.scss$/,
--- src/Typography/__tests__/Typography.test.tsx
+++ src/Typography/__tests__/Typography.test.tsx
@@ -1,18 +1,54 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 
-import Typography from '../Typography';
+import Typography, { defaultTypesMapping, Variant } from '../Typography';
 
-test('render', () => {
+test('render element', () => {
   const { getByText } = render(
-    <Typography variant="heading1">Hello there</Typography>
+    <Typography
+      variant="heading1"
+      data-cy="typo-heading1"
+      style={{ width: 200 }}
+    >
+      Hello
+    </Typography>
   );
 
-  expect(getByText('Hello there')).toMatchInlineSnapshot(`
+  expect(getByText('Hello')).toMatchInlineSnapshot(`
     <h1
       class="typography typography__heading1"
+      data-cy="typo-heading1"
+      style="width: 200px;"
     >
-      Hello there
+      Hello
     </h1>
   `);
+});
+
+test.each(Object.keys(defaultTypesMapping))(
+  'render `%s` variant class',
+  (variant) => {
+    const { getByText } = render(
+      <Typography variant={variant as Variant}>Hey</Typography>
+    );
+
+    expect(getByText('Hey')).toHaveClass(`typography typography__${variant}`);
+  }
+);
+
+test('render another class', () => {
+  const { getByText } = render(
+    <Typography
+      variant="paragraph"
+      data-cy="typo-heading1"
+      style={{ width: 200 }}
+      className="another-css-class__1 another-css-class__2"
+    >
+      My Text
+    </Typography>
+  );
+
+  expect(getByText('My Text')).toHaveClass(
+    `typography typography__paragraph another-css-class__1 another-css-class__2`
+  );
 });
