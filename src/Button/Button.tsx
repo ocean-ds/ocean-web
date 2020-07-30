400fdbf548f02b84bd348ef06d8e52fab249312a
Willian Gomes de Oliveira <willian.gomes@useblu.com.br>
2020-07-30 09:50:51 -0300

    button component ts

---
+++ src/Button/Button.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './styles/button.scss';
+
+/**
+ * Utility type
+ */
+
+type MergeElementProps<
+  T extends React.ElementType,
+  P extends Record<string, unknown>
+> = Omit<React.ComponentPropsWithRef<T>, keyof P> & P;
+
+export type ButtonProps<P extends React.ElementType = 'button'> = {
+  component?: P;
+} & MergeElementProps<
+  P,
+  {
+    children: React.ReactNode;
+
+    className?: string;
+
+    variant?: 'contained' | 'text' | 'blocked';
+
+    color?: 'primary' | 'secondary' | 'inverse';
+
+    size?: 'sm' | 'md' | 'lg';
+  }
+>;
+
+function ButtonBase<T extends React.ElementType = 'button'>(
+  {
+    children,
+    className,
+    type = 'button',
+    variant = 'contained',
+    color = 'primary',
+    size = 'md',
+    ...rest
+  }: ButtonProps<T>,
+  ref: React.Ref<HTMLButtonElement>
+) {
+  return React.createElement(
+    rest.component || 'button',
+    {
+      ref,
+      className: classNames(
+        `ods-button ods-button-${variant} ods-button-${color} ods-button-${size}`,
+        className
+      ),
+      type,
+      ...rest,
+    },
+    children
+  );
+}
+
+const Button = (React.forwardRef(ButtonBase) as unknown) as typeof ButtonBase;
+
+export default Button;
