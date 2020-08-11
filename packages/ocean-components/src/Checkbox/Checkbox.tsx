72c5dcec9cc1514eabedf263dc26874b6cdc91a4
Elton Garbin <eltongarbin@gmail.com>
2020-08-11 15:06:08 -0300

    feat: adds checkbox (#202)

---
+++ packages/ocean-components/src/Checkbox/Checkbox.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import classNames from 'classnames';
+
+import './styles/checkbox.scss';
+
+type CheckboxProps = {
+  /**
+   * The label content.
+   */
+  label?: React.ReactNode;
+} & React.ComponentPropsWithoutRef<'input'>;
+
+const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
+  function Checkbox({ className, label, id, ...rest }, ref) {
+    return (
+      <label className="ods-checkbox__root" htmlFor={id}>
+        <input
+          ref={ref}
+          id={id}
+          className={classNames('ods-checkbox', className)}
+          {...rest}
+          type="checkbox"
+        />
+        <span className="ods-checkbox__checkmark"></span>
+        {label && (
+          <span className="ods-typography ods-typography__paragraph ods-checkbox__label">
+            {label}
+          </span>
+        )}
+      </label>
+    );
+  }
+);
+
+export default Checkbox;
