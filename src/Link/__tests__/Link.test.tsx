4aada57e42e63454d21957848f7790e5f61c37c1
Elton Garbin <eltongarbin@gmail.com>
2020-08-14 14:50:18 -0300

    fix tests

--- src/Link/__tests__/Link.test.tsx
+++ src/Link/__tests__/Link.test.tsx
@@ -1,87 +1,34 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { MemoryRouter as Router, Link } from 'react-router-dom';
+import { MemoryRouter as Router, Link as RouterLink } from 'react-router-dom';
 
 import Link from '../Link';
 
 test('renders element properly', () => {
   const { getByTestId } = render(
     <Link data-testid="lnk-test" className="custom-class">
-      Hello
+      Click here!
     </Link>
   );
 
   expect(getByTestId('lnk-test')).toMatchInlineSnapshot(`
     <a
-      class="ods-lnk ods-lnk--md ods-lnk--primary custom-class"
+      class="ods-lnk custom-class"
       data-testid="lnk-test"
     >
-      Hello
+      Click here!
     </a>
   `);
 });
-
-test('renders a small a', () => {
-  const { getByTestId } = render(<Link data-testid="lnk-test" size="sm" />);
-  expect(getByTestId('lnk-test')).toHaveClass('ods-lnk--sm');
-});
-
-test('renders a medium a', () => {
-  const { getByTestId } = render(<Link data-testid="lnk-test" size="md" />);
-  expect(getByTestId('lnk-test')).toHaveClass('ods-lnk--md');
-});
-
-test('renders a large a', () => {
-  const { getByTestId } = render(<Link data-testid="lnk-test" size="lg" />);
-  expect(getByTestId('lnk-test')).toHaveClass('ods-lnk--lg');
-});
-
-test('renders primary a', () => {
-  const { getByTestId } = render(
-    <Link data-testid="lnk-test" variant="primary" />
-  );
-
-  expect(getByTestId('lnk-test').className).toBe(
-    'ods-lnk ods-lnk--md ods-lnk--primary'
-  );
-});
-
-test('renders secondary a', () => {
-  const { getByTestId } = render(
-    <Link data-testid="lnk-test" variant="secondary" />
-  );
-
-  expect(getByTestId('lnk-test').className).toBe(
-    'ods-lnk ods-lnk--md ods-lnk--secondary'
-  );
-});
-
-test('renders a text a', () => {
-  const { getByTestId } = render(
-    <Link data-testid="lnk-test" variant="text" />
-  );
-
-  expect(getByTestId('lnk-test').className).toBe(
-    'ods-lnk ods-lnk--md ods-lnk--text'
-  );
-});
-
-test('renders an inverse a', () => {
-  const { getByTestId } = render(
-    <Link data-testid="lnk-test" variant="inverse" />
-  );
+test('renders an inverse link', () => {
+  const { getByTestId } = render(<Link data-testid="lnk-test" inverse />);
   expect(getByTestId('lnk-test')).toHaveClass('ods-lnk--inverse');
 });
 
-test('renders a full width a', () => {
-  const { getByTestId } = render(<Link data-testid="lnk-test" blocked />);
-  expect(getByTestId('lnk-test')).toHaveClass('ods-lnk--blocked');
-});
-
-test('renders a link a', () => {
+test('renders a link with router', () => {
   const { getByTestId } = render(
     <Router>
-      <Link data-testid="lnk-test" component={Link} to="/teste/1234">
+      <Link data-testid="lnk-test" component={RouterLink} to="/teste/1234">
         Link
       </Link>
     </Router>
@@ -89,7 +36,7 @@ test('renders a link a', () => {
 
   expect(getByTestId('lnk-test')).toMatchInlineSnapshot(`
     <a
-      class="ods-lnk ods-lnk--md ods-lnk--primary"
+      class="ods-lnk"
       data-testid="lnk-test"
       href="/teste/1234"
     >
