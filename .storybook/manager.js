30740911c333b577ce083cfb0f1011dad33d8017
Elton Garbin <eltongarbin@gmail.com>
2020-08-17 22:26:43 -0300

    docs: storybook customizations for ocean-ds

---
+++ .storybook/manager.js
@@ -0,0 +1,12 @@
+import { addons } from '@storybook/addons';
+import { create } from '@storybook/theming/create';
+
+import logo from './assets/logo.png';
+
+addons.setConfig({
+  theme: create({
+    brandTitle: 'Ocean DS',
+    brandUrl: 'https://zeroheight.com/9c9b2b3aa/p/49e1f9-ocean-design-system',
+    brandImage: logo,
+  }),
+});
