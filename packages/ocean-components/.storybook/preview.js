b41ffbac27559b7a11dc39743c17a10b867056fe
Elton Garbin <eltongarbin@gmail.com>
2020-08-17 22:26:43 -0300

    docs: storybook customizations for ocean-ds

--- packages/ocean-components/.storybook/preview.js
+++ packages/ocean-components/.storybook/preview.js
@@ -1,6 +1,26 @@
 import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 
 export const parameters = {
+  viewport: {
+    viewports: {
+      galaxys5: {
+        name: 'Galaxy S5',
+        styles: {
+          height: '640px',
+          width: '360px',
+        },
+        type: 'mobile',
+      },
+      ipad: {
+        name: 'iPad',
+        styles: {
+          height: '1024px',
+          width: '768px',
+        },
+        type: 'tablet',
+      },
+    },
+  },
   docs: {
     container: DocsContainer,
     page: DocsPage,
