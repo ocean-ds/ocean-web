6bfc41e66c95cffc632780bcd6ce0e1c755b8059
Elton Garbin <eltongarbin@gmail.com>
2020-08-27 14:04:49 -0300

    wrapping select with form control component

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -1,24 +1,41 @@
 import React from 'react';
 import ReactSelect, { OptionTypeBase, Props } from 'react-select';
 
+import FormControl, { FormControlProps } from '../FormControl/FormControl';
+
 type SelectProps = {
   /**
    * Placeholder text for the select value
    * @default 'Selecione uma opção'
    */
   placeholder?: React.ReactNode;
-} & Props<OptionTypeBase>;
+} & Omit<FormControlProps, 'children'> &
+  Props<OptionTypeBase>;
 
 const Select: React.FC<SelectProps> = ({
   placeholder = 'Selecione uma opção',
+  label,
+  id,
+  helperText,
+  error,
+  blocked,
   ...rest
 }) => (
-  <ReactSelect
-    classNamePrefix="ods-select"
-    placeholder={placeholder}
-    isSearchable={false}
-    {...rest}
-  />
+  <FormControl
+    label={label}
+    htmlFor={id}
+    helperText={helperText}
+    error={error}
+    blocked={blocked}
+  >
+    <ReactSelect
+      classNamePrefix="ods-select"
+      placeholder={placeholder}
+      isSearchable={false}
+      inputId={id}
+      {...rest}
+    />
+  </FormControl>
 );
 
 export default Select;
