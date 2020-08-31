6754d9b55458d7e032bb2e96c543d78c52c35e3f
Elton Garbin <eltongarbin@gmail.com>
2020-08-31 10:59:32 -0300

    Adds FormControl component (#310)

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import FormControl, { FormControlProps } from '../FormControl/FormControl';
 import './styles/input.scss';
 
 type InputProps = {
@@ -9,60 +10,33 @@ type InputProps = {
    * @default 'text'
    */
   type?: 'text' | 'email' | 'number' | 'password' | 'search' | 'tel' | 'url';
-  /**
-   * The label content.
-   */
-  label?: React.ReactNode;
-  /**
-   * If `true`, the label will be displayed in an error state.
-   * @default false
-   */
-  error?: boolean;
-  /**
-   * The helper text content.
-   */
-  helperText?: React.ReactNode;
-  /**
-   * Spans the full width of the Input parent.
-   * @default false
-   */
-  blocked?: boolean;
-} & React.ComponentPropsWithoutRef<'input'>;
+} & Omit<FormControlProps, 'children'> &
+  React.ComponentPropsWithoutRef<'input'>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input(
   { type, className, label, helperText, blocked, error, id, ...rest },
   ref
 ) {
   return (
-    <div className="ods-input__root">
-      {label && (
-        <label className="ods-input__label" htmlFor={id}>
-          {label}
-        </label>
-      )}
+    <FormControl
+      label={label}
+      htmlFor={id}
+      helperText={helperText}
+      error={error}
+      blocked={blocked}
+    >
       <input
         ref={ref}
         type={type || 'text'}
         id={id}
         className={classNames(
           'ods-input',
-          blocked && 'ods-input--blocked',
           error && 'ods-input--error',
           className
         )}
         {...rest}
       />
-      {helperText && (
-        <p
-          className={classNames(
-            'ods-input__helper-text',
-            error && 'ods-input__helper-text--error'
-          )}
-        >
-          {helperText}
-        </p>
-      )}
-    </div>
+    </FormControl>
   );
 });
 
--- src/Input/__tests__/Input.test.tsx
+++ src/Input/__tests__/Input.test.tsx
@@ -10,45 +10,27 @@ test('renders element properly', () => {
 
   expect(container.firstChild).toMatchInlineSnapshot(`
     <div
-      class="ods-input__root"
+      class="ods-form-control__root"
     >
-      <input
-        class="ods-input custom-class"
-        data-testid="input-test"
-        type="text"
-      />
+      <div
+        class="ods-form-control__element"
+      >
+        <input
+          class="ods-input custom-class"
+          data-testid="input-test"
+          type="text"
+        />
+      </div>
     </div>
   `);
   expect(getByTestId('input-test')).toHaveAttribute('type', 'text');
 });
 
-test('renders a label for the input', () => {
-  const { getByText } = render(<Input label="The input label" />);
-  expect(getByText('The input label').className).toBe('ods-input__label');
-});
-
-test('renders a helper text for the input', () => {
-  const { getByText } = render(<Input helperText="Just a small info." />);
-  expect(getByText('Just a small info.').className).toBe(
-    'ods-input__helper-text'
-  );
-});
-
-test('renders a full width input', () => {
-  const { getByTestId } = render(<Input data-testid="input-test" blocked />);
-  expect(getByTestId('input-test').className).toBe(
-    'ods-input ods-input--blocked'
-  );
-});
-
 test('renders a error state for the input', () => {
-  const { getByTestId, getByText } = render(
+  const { getByTestId } = render(
     <Input data-testid="input-test" error helperText="Error message." />
   );
   expect(getByTestId('input-test').className).toBe(
     'ods-input ods-input--error'
-  );
-  expect(getByText('Error message.').className).toBe(
-    'ods-input__helper-text ods-input__helper-text--error'
   );
 });
