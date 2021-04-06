bf9a79b792e06882a7ee365950ecd5e7775c239c
Elton Garbin <eltongarbin@gmail.com>
2021-04-06 14:37:33 -0300

    feat: change from ocean-components to ocean-core (#613)
    BREAKING CHANGE: change pkg name and export only css files

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,8 @@
-const path = require('path');
-
 module.exports = {
   stories: ['../packages/*/src/**/*.stories.mdx'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-a11y'],
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
-    });
-
-    return config;
-  },
+  addons: [
+    '@storybook/preset-scss',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+  ],
 };
