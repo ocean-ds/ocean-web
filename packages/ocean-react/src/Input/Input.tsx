daa7296818c36f76027f811a42f54956391800ce
Elton Garbin <eltongarbin@gmail.com>
2021-08-11 17:22:20 -0300

    feat(input): add filled state

--- packages/ocean-react/src/Input/Input.tsx
+++ packages/ocean-react/src/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import classNames from 'classnames';
 
 import FormControl, { FormControlProps } from '../FormControl';
@@ -13,9 +13,42 @@ export type InputProps = {
   React.ComponentPropsWithoutRef<'input'>;
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input(
-  { type, className, label, helperText, blocked, error, id, disabled, ...rest },
+  {
+    type,
+    className,
+    label,
+    helperText,
+    blocked,
+    error,
+    id,
+    disabled,
+    onChange,
+    value = undefined,
+    defaultValue,
+    ...rest
+  },
   ref
 ) {
+  const [filled, setFilled] = useState(Boolean(value || defaultValue));
+
+  const handleChange = useCallback(
+    (event) => {
+      if (onChange) return onChange(event);
+
+      // uncontrolled version
+      setFilled(Boolean(event.target.value));
+    },
+    [onChange]
+  );
+
+  useEffect(() => {
+    setFilled(Boolean(value));
+  }, [value]);
+
+  useEffect(() => {
+    setFilled(Boolean(defaultValue));
+  }, [defaultValue]);
+
   return (
     <FormControl
       label={label}
@@ -31,10 +64,14 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input(
         id={id}
         className={classNames(
           'ods-input',
+          filled && 'ods-input--filled',
           error && 'ods-input--error',
           className
         )}
         disabled={disabled}
+        onChange={handleChange}
+        defaultValue={defaultValue}
+        value={value}
         {...rest}
       />
     </FormControl>
