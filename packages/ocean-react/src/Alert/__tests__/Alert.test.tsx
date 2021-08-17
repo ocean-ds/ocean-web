b361fafe4503fc3499945b594eb9577dce546609
Leonardo Ferreira <leualemax@gmail.com>
2021-08-17 08:16:36 -0300

    fix(alert): adds a wrapper for content to let user to have how many children they want

--- packages/ocean-react/src/Alert/__tests__/Alert.test.tsx
+++ packages/ocean-react/src/Alert/__tests__/Alert.test.tsx
@@ -9,106 +9,162 @@ jest.mock('@useblu/ocean-icons-react', () => ({
   ExclamationCircleOutline: () => 'mock-exclamation-circle-outline',
   XCircleOutline: () => 'mock-x-circle-outline',
   CheckCircleOutline: () => 'mock-check-circle-outline',
-  StarOutline: () => 'mock-start-outline',
+  StarOutline: function StarOutlineMock(
+    props: React.HTMLAttributes<HTMLDivElement>
+  ) {
+    return <div {...props}>mock-start-circle-outline</div>;
+  },
 }));
 
 const setup = (props?: AlertProps) => {
-  render(<Alert {...props}>Hello There!</Alert>);
+  render(
+    <Alert {...props}>
+      <div className="ods-alert__content">Hello There!</div>
+    </Alert>
+  );
 };
 
 test('renders default element properly', () => {
   setup();
 
   expect(document.querySelector('.ods-alert')).toMatchInlineSnapshot(`
+<div
+  class="ods-alert ods-alert--default"
+  role="alert"
+>
+  mock-information-circle-outline
+  <div
+    class="ods-alert__content"
+  >
     <div
-      class="ods-alert ods-alert--default"
-      role="alert"
+      class="ods-alert__content"
     >
-      mock-information-circle-outline
       Hello There!
     </div>
-  `);
+  </div>
+</div>
+`);
 });
 
 test('renders success element properly', () => {
   setup({ type: 'success' });
 
   expect(document.querySelector('.ods-alert')).toMatchInlineSnapshot(`
+<div
+  class="ods-alert ods-alert--success"
+  role="alert"
+>
+  mock-check-circle-outline
+  <div
+    class="ods-alert__content"
+  >
     <div
-      class="ods-alert ods-alert--success"
-      role="alert"
+      class="ods-alert__content"
     >
-      mock-check-circle-outline
       Hello There!
     </div>
-  `);
+  </div>
+</div>
+`);
 });
 
 test('renders error element properly', () => {
   setup({ type: 'error' });
 
   expect(document.querySelector('.ods-alert')).toMatchInlineSnapshot(`
+<div
+  class="ods-alert ods-alert--error"
+  role="alert"
+>
+  mock-x-circle-outline
+  <div
+    class="ods-alert__content"
+  >
     <div
-      class="ods-alert ods-alert--error"
-      role="alert"
+      class="ods-alert__content"
     >
-      mock-x-circle-outline
       Hello There!
     </div>
-  `);
+  </div>
+</div>
+`);
 });
 
 test('renders element with a custom icon', () => {
   setup({ icon: <StarOutline /> });
 
   expect(document.querySelector('.ods-alert')).toMatchInlineSnapshot(`
+<div
+  class="ods-alert ods-alert--default"
+  role="alert"
+>
+  <div
+    class="ods-alert__icon"
+  >
+    mock-start-circle-outline
+  </div>
+  <div
+    class="ods-alert__content"
+  >
     <div
-      class="ods-alert ods-alert--default"
-      role="alert"
+      class="ods-alert__content"
     >
-      <div
-        class="ods-alert__icon"
-      >
-        mock-start-outline
-      </div>
       Hello There!
     </div>
-  `);
+  </div>
+</div>
+`);
 });
 
 test('renders warning element properly', () => {
   setup({ type: 'warning' });
 
   expect(document.querySelector('.ods-alert')).toMatchInlineSnapshot(`
+<div
+  class="ods-alert ods-alert--warning"
+  role="alert"
+>
+  mock-exclamation-circle-outline
+  <div
+    class="ods-alert__content"
+  >
     <div
-      class="ods-alert ods-alert--warning"
-      role="alert"
+      class="ods-alert__content"
     >
-      mock-exclamation-circle-outline
       Hello There!
     </div>
-  `);
+  </div>
+</div>
+`);
 });
 
 test('renders element properly with title', () => {
   setup({ title: "Test's Title" });
 
   expect(document.querySelector('.ods-alert')).toMatchInlineSnapshot(`
+<div
+  class="ods-alert ods-alert--default ods-alert--has-header"
+  role="alert"
+>
+  <div
+    class="ods-alert__header"
+  >
+    mock-information-circle-outline
     <div
-      class="ods-alert ods-alert--default ods-alert--has-header"
-      role="alert"
+      class="ods-alert__title"
     >
-      <div
-        class="ods-alert__header"
-      >
-        mock-information-circle-outline
-        <div
-          class="ods-alert__title"
-        >
-          Test's Title
-        </div>
-      </div>
+      Test's Title
+    </div>
+  </div>
+  <div
+    class="ods-alert__content"
+  >
+    <div
+      class="ods-alert__content"
+    >
       Hello There!
     </div>
-  `);
+  </div>
+</div>
+`);
 });
