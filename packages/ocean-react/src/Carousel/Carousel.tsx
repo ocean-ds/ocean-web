d4e0594c95ed539f298ec08cd4c1de3728d22d9e
lauro <laurolyra@gmail.com>
2021-10-01 09:08:02 -0300

    fix: add scroll buttons

--- packages/ocean-react/src/Carousel/Carousel.tsx
+++ packages/ocean-react/src/Carousel/Carousel.tsx
@@ -1,5 +1,6 @@
 import React, { Children, useRef } from 'react';
 import classNames from 'classnames';
+import { ChevronLeft, ChevronRight } from '@useblu/ocean-icons-react';
 
 export type CarouselProps = {
   maxPerSection: '1' | '2' | '3' | '4' | '5';
@@ -21,31 +22,42 @@ const Carousel = React.forwardRef<HTMLDivElement, CarouselProps>(
 
     const handleLeftClick = (e: React.MouseEvent<HTMLElement>) => {
       e.preventDefault();
-      carousel.current.scrollLeft -= refPixels[maxPerSection];
+      carousel.current.scrollLeft = Math.ceil(
+        (carousel.current.scrollLeft -= 1298)
+      );
     };
 
     const handleRightClick = (e: React.MouseEvent<HTMLElement>) => {
       e.preventDefault();
-      carousel.current.scrollLeft += refPixels[maxPerSection] + 16;
+      carousel.current.scrollLeft = Math.ceil(
+        (carousel.current.scrollLeft += 1298)
+      );
     };
 
     return (
       <>
-        <div className={classNames('ods-carousel-container')} ref={carousel}>
-          {/* {count} */}
-          {Children.toArray(children).map((child, index) => (
-            <div
-              key={index}
-              className={classNames(
-                `ods-carousel-item-maxPerSection--${maxPerSection}`
-              )}
-            >
-              {child}
-            </div>
-          ))}
+        <div className={classNames('ods-carousel-and-buttons')}>
+          <button onClick={handleLeftClick}>
+            <ChevronLeft />
+          </button>
+          <div className={classNames('ods-carousel-container')} ref={carousel}>
+            {/* {count} */}
+            {Children.toArray(children).map((child, index) => (
+              <div
+                key={index}
+                className={classNames(
+                  'ods-carousel-item-maxPerSection',
+                  `ods-carousel-item-maxPerSection--${maxPerSection}`
+                )}
+              >
+                {child}
+              </div>
+            ))}
+          </div>
+          <button onClick={handleRightClick}>
+            <ChevronRight />
+          </button>
         </div>
-        <button onClick={handleLeftClick}>Left</button>
-        <button onClick={handleRightClick}>Right</button>
         <div className={classNames('ods-carousel-pagination-container')}>
           <div className={classNames('ods-carousel-page')}></div>
           <div className={classNames('ods-carousel-page')}></div>
