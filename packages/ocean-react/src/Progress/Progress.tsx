b6ad669826a6d0fd1240fd41988adcd76e67bf02
galvaodev <galvaopp@gmail.com>
2021-10-26 16:01:54 -0300

    feat: create new component progress

---
+++ packages/ocean-react/src/Progress/Progress.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export type ProgressProps = {
+  /**
+   * The size of the progress.
+   * @default 'md'
+   */
+  size?: 'md' | 'lg' | 'sm';
+};
+
+const Progress: React.FC<ProgressProps> = ({ size = 'md', ...rest }) => {
+  return (
+    <div className="ods-progress" {...rest}>
+      <span className={`ods-progress__primary ods-progress--${size}`}>
+        <svg viewBox="22 22 44 44">
+          <circle cx="44" cy="44" r="18" fill="none" />
+        </svg>
+      </span>
+      <span className={`ods-progress__secondary ods-progress--${size}`}>
+        <svg viewBox="22 22 44 44">
+          <circle cx="44" cy="44" r="18" fill="none" />
+        </svg>
+      </span>
+    </div>
+  );
+};
+
+export default Progress;
