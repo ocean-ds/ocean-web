4fd2beb1d6fe0f556f11a0876ac1e37d6f2ae4fa
João Victor <galvaopp@gmail.com>
2021-12-17 13:57:10 -0300

    feat: new component topbar (#863)
    * feat: create new component top bar
    
    * feat: new component topBar
    
    * chore: update styles line and import component
    
    * chore: update coverage
    
    * chore: update coverage
    
    Co-authored-by: Leonardo "Alemax" Fonseca Ferreira <leualemax@gmail.com>

---
+++ packages/ocean-react/src/TopBar/TopBar.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { ArrowLeftOutline, SearchOutline } from '@useblu/ocean-icons-react';
+import classNames from 'classnames';
+
+interface TopBarProps {
+  onSearch?: () => void;
+  onBack?: () => void;
+  title: string;
+  description?: string;
+  variants?: 'default' | 'extended';
+  color?: 'light' | 'default';
+}
+
+const TopBar: React.FC<TopBarProps> = ({
+  onBack,
+  onSearch,
+  title,
+  description,
+  variants = 'default',
+  color = 'default',
+}) => {
+  return (
+    <div
+      className={classNames(
+        variants === 'default' ? 'ods-topbar' : 'ods-topbar ods-topbar-extend',
+        color === 'light' ? 'ods-topbar-light' : 'ods-topbar-default'
+      )}
+    >
+      {onBack && (
+        <div className="ods-topbar-prev">
+          <ArrowLeftOutline onClick={onBack} />
+        </div>
+      )}
+
+      {variants === 'extended' && (
+        <div className="ods-topbar-title">
+          Title
+          <span>Description</span>
+        </div>
+      )}
+
+      {onSearch && (
+        <div className="ods-topbar-search">
+          <SearchOutline onClick={onSearch} />
+        </div>
+      )}
+
+      {variants === 'default' && (
+        <div className="ods-topbar-title">
+          {title}
+          {description && <span>{description}</span>}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TopBar;
