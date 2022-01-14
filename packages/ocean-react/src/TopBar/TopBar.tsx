ae8da20514187631ce2ab977bc036ad083b68b06
galvaodev <galvaopp@gmail.com>
2022-01-14 12:57:29 -0300

    chore: update colors, variant and spacing

--- packages/ocean-react/src/TopBar/TopBar.tsx
+++ packages/ocean-react/src/TopBar/TopBar.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { ArrowLeftOutline, SearchOutline } from '@useblu/ocean-icons-react';
+import React, { ReactElement } from 'react';
+import { SearchOutline } from '@useblu/ocean-icons-react';
 import classNames from 'classnames';
 
 interface TopBarProps {
   onSearch?: () => void;
   onBack?: () => void;
+  leftIcon: ReactElement;
   title: string;
   description?: string;
   variants?: 'default' | 'extended';
-  color?: 'light' | 'default';
+  color?: 'off' | 'on';
 }
 
 const TopBar: React.FC<TopBarProps> = ({
   onBack,
   onSearch,
+  leftIcon,
   title,
   description,
   variants = 'default',
@@ -23,19 +25,19 @@ const TopBar: React.FC<TopBarProps> = ({
     <div
       className={classNames(
         variants === 'default' ? 'ods-topbar' : 'ods-topbar ods-topbar-extend',
-        color === 'light' ? 'ods-topbar-light' : 'ods-topbar-default'
+        color === 'off' ? 'ods-topbar-light' : 'ods-topbar-default'
       )}
     >
-      {onBack && (
+      {onBack && leftIcon && (
         <div className="ods-topbar-prev">
-          <ArrowLeftOutline onClick={onBack} />
+          <span onClick={onBack}>{leftIcon}</span>
         </div>
       )}
 
-      {variants === 'extended' && (
+      {variants === 'default' && (
         <div className="ods-topbar-title">
-          Title
-          <span>Description</span>
+          {title}
+          {description && <span>{description}</span>}
         </div>
       )}
 
@@ -45,7 +47,7 @@ const TopBar: React.FC<TopBarProps> = ({
         </div>
       )}
 
-      {variants === 'default' && (
+      {variants === 'extended' && (
         <div className="ods-topbar-title">
           {title}
           {description && <span>{description}</span>}
