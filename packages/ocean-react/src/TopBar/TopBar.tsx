e14764301b07f252f0eccd5fd45a51b0d6dad88d
galvaodev <galvaopp@gmail.com>
2022-01-17 15:35:27 -0300

    chore: update scroll bar class

--- packages/ocean-react/src/TopBar/TopBar.tsx
+++ packages/ocean-react/src/TopBar/TopBar.tsx
@@ -25,7 +25,9 @@ const TopBar: React.FC<TopBarProps> = ({
     <div
       className={classNames(
         variants === 'default' ? 'ods-topbar' : 'ods-topbar ods-topbar-extend',
-        color === 'on' ? 'ods-topbar-light' : 'ods-topbar-default'
+        color === 'on'
+          ? 'ods-topbar-light'
+          : 'ods-topbar-default ods-topbar-scroll-bar'
       )}
     >
       {onBack && leftIcon && (
