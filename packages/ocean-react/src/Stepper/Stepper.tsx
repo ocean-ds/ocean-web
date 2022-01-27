2f28c66bb6f72738b4b359a7965227b335141f2e
joaopedrorc <jpc5626@gmail.com>
2022-01-27 11:47:23 -0300

    feat: stepper component done

--- packages/ocean-react/src/Stepper/Stepper.tsx
+++ packages/ocean-react/src/Stepper/Stepper.tsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { ReactElement, Fragment } from 'react';
 
-interface StepperProps {
-  step: number;
-  off: number;
+export interface StepperProps {
+  /**
+   * Determines the current step.
+   * @default 1
+   */
+  step: 1 | 2 | 3 | 4;
+
+  /**
+   * Determines the total amount of steps.
+   * @default 4
+   */
+  off: 1 | 2 | 3 | 4;
 }
+const defaultProps = {
+  step: 1,
+  off: 4,
+};
 
-// off = 4
-
-const Stepper = ({ step, off }: StepperProps) => {
-  const color = '#13BDBD';
-
+const Stepper = ({
+  step,
+  off,
+}: StepperProps & typeof defaultProps): ReactElement => {
   if (step === 1) {
     return (
       <svg
@@ -18,20 +30,21 @@ const Stepper = ({ step, off }: StepperProps) => {
         viewBox="0 0 116 20"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        data-testid="stepper-1"
       >
-        <circle cx="9.5" cy="10.5" r="5.5" stroke={color} strokeWidth="2" />
-        <path d="M15 10H20" stroke={color} strokeWidth="2" />
+        <circle cx="9.5" cy="10.5" r="5.5" stroke="#13BDBD" strokeWidth="2" />
+        <path d="M15 10H20" stroke="#13BDBD" strokeWidth="2" />
 
         {off === 2 && (
-          <>
+          <Fragment>
             <path d="M20 10H32" stroke="#EBECF5" strokeWidth="2" />
             <path d="M32 10L37 10" stroke="#EBECF5" strokeWidth="2" />
             <circle cx="42" cy="10" r="5" stroke="#EBECF5" strokeWidth="2" />
-          </>
+          </Fragment>
         )}
 
         {off === 3 && (
-          <>
+          <Fragment>
             <path d="M20 10H32" stroke="#EBECF5" strokeWidth="2" />
             <path d="M32 10L37 10" stroke="#EBECF5" strokeWidth="2" />
             <circle cx="42" cy="10" r="5" stroke="#EBECF5" strokeWidth="2" />
@@ -39,11 +52,11 @@ const Stepper = ({ step, off }: StepperProps) => {
             <path d="M52 10H64" stroke="#EBECF5" strokeWidth="2" />
             <path d="M64 10L69 10" stroke="#EBECF5" strokeWidth="2" />
             <circle cx="74" cy="10" r="5" stroke="#EBECF5" strokeWidth="2" />
-          </>
+          </Fragment>
         )}
 
         {off === 4 && (
-          <>
+          <Fragment>
             <path d="M20 10H32" stroke="#EBECF5" strokeWidth="2" />
             <path d="M32 10L37 10" stroke="#EBECF5" strokeWidth="2" />
             <circle cx="42" cy="10" r="5" stroke="#EBECF5" strokeWidth="2" />
@@ -60,7 +73,7 @@ const Stepper = ({ step, off }: StepperProps) => {
               strokeWidth="2"
             />
             <path d="M101 10H96" stroke="#EBECF5" strokeWidth="2" />
-          </>
+          </Fragment>
         )}
       </svg>
     );
@@ -74,6 +87,7 @@ const Stepper = ({ step, off }: StepperProps) => {
         viewBox="0 0 116 20"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        data-testid="stepper-2"
       >
         <circle
           cx="10"
@@ -94,23 +108,22 @@ const Stepper = ({ step, off }: StepperProps) => {
         <path d="M32 10L37 10" stroke="#13BDBD" strokeWidth="2" />
         <circle cx="42" cy="10" r="5" stroke="#13BDBD" strokeWidth="2" />
         {off > 2 && (
-          <>
-            {' '}
+          <Fragment>
             <path d="M47 10H52" stroke="#13BDBD" strokeWidth="2" />
             <path d="M52 10H64" stroke="#EBECF5" strokeWidth="2" />
-          </>
+          </Fragment>
         )}
 
         {off === 3 && (
-          <>
+          <Fragment>
             <path d="M52 10H64" stroke="#EBECF5" strokeWidth="2" />
             <path d="M64 10L69 10" stroke="#EBECF5" strokeWidth="2" />
             <circle cx="74" cy="10" r="5" stroke="#EBECF5" strokeWidth="2" />
-          </>
+          </Fragment>
         )}
 
         {off === 4 && (
-          <>
+          <Fragment>
             <path d="M52 10H64" stroke="#EBECF5" strokeWidth="2" />
             <path d="M64 10L69 10" stroke="#EBECF5" strokeWidth="2" />
             <circle cx="74" cy="10" r="5" stroke="#EBECF5" strokeWidth="2" />
@@ -123,7 +136,7 @@ const Stepper = ({ step, off }: StepperProps) => {
               strokeWidth="2"
             />
             <path d="M101 10H96" stroke="#EBECF5" strokeWidth="2" />
-          </>
+          </Fragment>
         )}
       </svg>
     );
@@ -137,6 +150,7 @@ const Stepper = ({ step, off }: StepperProps) => {
         viewBox="0 0 116 20"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
+        data-testid="stepper-3"
       >
         <circle
           cx="10"
@@ -173,7 +187,7 @@ const Stepper = ({ step, off }: StepperProps) => {
         <path d="M64 10L69 10" stroke="#13BDBD" strokeWidth="2" />
         <circle cx="74" cy="10" r="5" stroke="#13BDBD" strokeWidth="2" />
         {off > 3 && (
-          <>
+          <Fragment>
             <path d="M79 10H84" stroke="#13BDBD" strokeWidth="2" />
             <path d="M84 10H96" stroke="#EBECF5" strokeWidth="2" />
             <path d="M101 10H96" stroke="#EBECF5" strokeWidth="2" />
@@ -183,76 +197,8 @@ const Stepper = ({ step, off }: StepperProps) => {
               stroke="#EBECF5"
               strokeWidth="2"
             />
-          </>
+          </Fragment>
         )}
-      </svg>
-    );
-  }
-
-  if (step === 4) {
-    return (
-      <svg
-        width="116"
-        height="20"
-        viewBox="0 0 116 20"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <circle
-          cx="10"
-          cy="10"
-          r="9"
-          fill="#13BDBD"
-          stroke="#13BDBD"
-          strokeWidth="2"
-        />
-        <path
-          d="M7 10L9 12L13 8"
-          stroke="white"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path d="M20 10H32" stroke="#13BDBD" strokeWidth="2" />
-        <circle
-          cx="42"
-          cy="10"
-          r="9"
-          fill="#13BDBD"
-          stroke="#13BDBD"
-          strokeWidth="2"
-        />
-        <path
-          d="M39 10L41 12L45 8"
-          stroke="white"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path d="M52 10H64" stroke="#13BDBD" strokeWidth="2" />
-        <circle
-          cx="74"
-          cy="10"
-          r="9"
-          fill="#13BDBD"
-          stroke="#13BDBD"
-          strokeWidth="2"
-        />
-        <path
-          d="M71 10L73 12L77 8"
-          stroke="white"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        />
-        <path d="M84 10H96" stroke="#13BDBD" strokeWidth="2" />
-        <path d="M101 10H96" stroke="#13BDBD" strokeWidth="2" />
-        <circle
-          r="5"
-          transform="matrix(-1 0 0 1 106 10)"
-          stroke="#13BDBD"
-          strokeWidth="2"
-        />
       </svg>
     );
   }
@@ -264,6 +210,7 @@ const Stepper = ({ step, off }: StepperProps) => {
       viewBox="0 0 116 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      data-testid="stepper-4"
     >
       <circle
         cx="10"
@@ -281,21 +228,45 @@ const Stepper = ({ step, off }: StepperProps) => {
         strokeLinejoin="round"
       />
       <path d="M20 10H32" stroke="#13BDBD" strokeWidth="2" />
-      <path d="M32 10L37 10" stroke="#13BDBD" strokeWidth="2" />
-      <circle cx="42" cy="10" r="5" stroke="#13BDBD" strokeWidth="2" />
-      <path d="M47 10H52" stroke="#13BDBD" strokeWidth="2" />
-      <path d="M52 10H64" stroke="#EBECF5" strokeWidth="2" />
-      <path d="M64 10L69 10" stroke="#EBECF5" strokeWidth="2" />
-      <circle cx="74" cy="10" r="5" stroke="#EBECF5" strokeWidth="2" />
-      <path d="M79 10H84" stroke="#EBECF5" strokeWidth="2" />
-      <path d="M84 10H96" stroke="#EBECF5" strokeWidth="2" />
+      <circle
+        cx="42"
+        cy="10"
+        r="9"
+        fill="#13BDBD"
+        stroke="#13BDBD"
+        strokeWidth="2"
+      />
+      <path
+        d="M39 10L41 12L45 8"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M52 10H64" stroke="#13BDBD" strokeWidth="2" />
+      <circle
+        cx="74"
+        cy="10"
+        r="9"
+        fill="#13BDBD"
+        stroke="#13BDBD"
+        strokeWidth="2"
+      />
+      <path
+        d="M71 10L73 12L77 8"
+        stroke="white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path d="M84 10H96" stroke="#13BDBD" strokeWidth="2" />
+      <path d="M101 10H96" stroke="#13BDBD" strokeWidth="2" />
       <circle
         r="5"
         transform="matrix(-1 0 0 1 106 10)"
-        stroke="#EBECF5"
+        stroke="#13BDBD"
         strokeWidth="2"
       />
-      <path d="M101 10H96" stroke="#EBECF5" strokeWidth="2" />
     </svg>
   );
 };
---
+++ packages/ocean-react/src/Stepper/__tests__/Stepper.test.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Stepper, { StepperProps } from '../Stepper';
+
+const setup = (props: StepperProps = { step: 1, off: 4 }) => {
+  return render(<Stepper {...props} />);
+};
+
+test.each([1, 2, 3, 4] as const)('renders each step option`', (step) => {
+  setup({ step, off: 4 });
+
+  expect(screen.getByTestId(`stepper-${step}`)).toBeInTheDocument();
+});
+
+test('renders the first step off two', () => {
+  setup({ step: 1, off: 2 });
+
+  expect(document.firstChild).toMatchInlineSnapshot(`DocumentType {}`);
+});
+
+test('renders the first step off three', () => {
+  setup({ step: 1, off: 3 });
+
+  expect(document.firstChild).toMatchInlineSnapshot(`DocumentType {}`);
+});
+
+test('renders the second step off three', () => {
+  setup({ step: 2, off: 3 });
+
+  expect(document.firstChild).toMatchInlineSnapshot(`DocumentType {}`);
+});
