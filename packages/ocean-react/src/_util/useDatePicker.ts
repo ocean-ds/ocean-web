3cedeee87c6ea4ed161aa9022b0bb65b143c5235
Fabio_Oliveira <fabio.oliveira@useblu.com.br>
2022-09-06 15:56:29 -0300

    fix: click input logical

--- packages/ocean-react/src/_util/useDatePicker.ts
+++ packages/ocean-react/src/_util/useDatePicker.ts
@@ -71,13 +71,9 @@ export default function useDatePicker({
     const formattedDay = DateFns.format(day, localeDateFormat);
 
     if (isSelectingLastDay) {
-      if (day < fromDate) {
-        updateState({ from: formattedDay, to: '' });
-      } else {
-        setIsSelectingLastDay(false);
-        setShowDayPicker(false);
-        updateState({ from: values.from, to: formattedDay });
-      }
+      setIsSelectingLastDay(false);
+      setShowDayPicker(false);
+      updateState({ from: values.from, to: formattedDay });
     } else {
       setIsSelectingLastDay(true);
       updateState({ from: formattedDay, to: '' });
