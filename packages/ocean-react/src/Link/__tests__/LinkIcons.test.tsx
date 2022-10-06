004429f71893e220c62ac8831288a6736d9993b0
João Pedro Cordeiro <jpc5626@gmail.com>
2022-10-06 08:34:10 -0300

    fix: correcting camelcase spelling of prop and adding a span attribute (#1019)
    * fix: correcting camelcase spelling of prop and adding a span attribute
    
    * fix: updates unit tests snapshots

--- packages/ocean-react/src/Link/__tests__/LinkIcons.test.tsx
+++ packages/ocean-react/src/Link/__tests__/LinkIcons.test.tsx
@@ -4,8 +4,8 @@ import { render, screen } from '@testing-library/react';
 import LinkIcons from '../LinkIcons';
 
 describe('renders the LinkIcons component with both icons', () => {
-  test('should render the LinkIcons component with LinkChevron icon', () => {
-    render(<LinkIcons icon="LinkChevron" />);
+  test('should render the LinkIcons component with linkChevron icon', () => {
+    render(<LinkIcons icon="linkChevron" />);
 
     expect(screen.queryByTestId('link-icon')).toBeInTheDocument();
     expect(screen.queryByTestId('link-icon')).toHaveClass('ods-link__icon');
