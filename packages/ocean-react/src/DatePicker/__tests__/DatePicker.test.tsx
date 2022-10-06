f91b2c4ee160ab035c65afb7fa80bce886cb4d78
FabioRolin <92738259+FabioRolin@users.noreply.github.com>
2022-10-06 18:29:26 -0300

    feat: datepicker (#1007)
    * feat: add date picker component
    
    * feat: add date picker hook
    
    * feat: add circle in start and end range date
    
    * feat: add mdx to date picker
    
    * feat: fix format of dates in date picker
    
    * feat: fix days style by state
    
    * chore: add locale config and adjust in date mask
    
    * feat: fix styles
    
    * chore: add tests
    
    * feat: fix tests
    
    * feat: fix documentation and fix component bug
    
    * feat: fix i18n statment
    
    * chore: fix placeholder text
    
    * chore: fix startToday atributte name
    
    * fix: remove width atributte duplication
    
    * fix: format scss file
    
    * fix: scss format
    
    * fix: scss fix
    
    * fix: scss identation fix
    
    * fix: change scss logical to pass CI pipeline
    
    * fix: remove wrong scss code
    
    * fix: add validations to coverage
    
    * test: add locale test
    
    * test: add disabled days test
    
    * fix: refactory code
    
    * fix: click input logical
    
    * revert: fix bug with refactory
    
    * chore: fix calendar header color
    
    * chore: fix calendar border radius
    
    * chore: fix icon buttons and set toggle calendar mode
    
    * chore: fix layout adjusts
    
    * fix: adjust css order attributes
    
    * fix: adjust css order after layout adjust
    
    * fix: adjust code
    
    * fix: adjust code coverage
    
    * fix: adjust code organization
    
    * fix: add missing tests
    
    * chore: remove wrong code
    
    * fix: remove unused function
    
    * chore: add adjust in coverage
    
    * chore: update doc and add parameter in props story
    
    * fix: adjust caption text
    
    * chore: adjust outside click to close calendar
    
    * fix: adjust days hover
    
    * fix: adjust date picker adorment to receive click
    
    * fix: adjust outline days hover
    
    * fix: adjust layout
    
    * chore: add input by click editable
    
    * fix: adjust test
    
    * fix: adjust tests
    
    * chore: enable any input editable without step
    
    * fix: adjust calendar anchor in mobile layout
    
    * fix: remove comment
    
    * fix: remove wrong code
    
    * fix: adjust test
    
    * fix: add support to numeric keyboard
    
    * chore: fix hover to when click outside not update data
    
    * fix: adjust startToday statment
    
    Co-authored-by: Saulo Santiago <saulodasilvasantiago@gmail.com>
    Co-authored-by: João Pedro Cordeiro <jpc5626@gmail.com>

---
+++ packages/ocean-react/src/DatePicker/__tests__/DatePicker.test.tsx
@@ -0,0 +1,743 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+import ptBr from 'date-fns/locale/pt-BR';
+
+import DatePicker from '../DatePicker';
+
+import { format } from 'date-fns';
+
+import { enUS } from 'date-fns/locale';
+
+test('renders element properly', () => {
+  const { container } = render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '10/08/2022', to: '11/08/2022' }}
+      onSelect={() => jest.fn()}
+      className="class-test"
+    />
+  );
+
+  expect(container.firstChild).toMatchInlineSnapshot(`
+  <div>
+    <div
+      class="ods-datepicker-background"
+      data-testid="date-picker-outside"
+    />
+    <div
+      class="ods-datepicker class-test"
+    >
+      <div
+        class="ods-datepicker__form-row"
+      >
+        <div
+          class="ods-datepicker__form-controls"
+          data-testid="date-picker-first-field-wrapper"
+        >
+          <label
+            for="start-date"
+          >
+            first-label
+          </label>
+          <div
+            class="ods-form-control__root"
+          >
+            <div
+              class="ods-form-control__header"
+            >
+              <div
+                class="ods-form-control__label"
+              />
+            </div>
+            <div
+              class="ods-form-control__element"
+            >
+              <div
+                class="ods-input ods-input--disabled--text"
+              >
+                <input
+                  autocomplete="off"
+                  data-testid="datepicker-input-1"
+                  id="start-date"
+                  inputmode="numeric"
+                  maxlength="10"
+                  name="start-date"
+                  pattern="[0-9]*"
+                  placeholder="dd/mm/aaaa"
+                  readonly=""
+                  type="text"
+                  value="10/08/2022"
+                />
+                <div
+                  class="ods-input__adornment ods-input__adornment--placeholder"
+                >
+                  <svg
+                    fill="none"
+                    height="20"
+                    stroke="#B6B9CC"
+                    viewBox="0 0 24 24"
+                    width="20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="ods-datepicker__form-controls"
+          data-testid="date-picker-second-field-wrapper"
+        >
+          <label
+            for="end-date"
+          >
+            second-label
+          </label>
+          <div
+            class="ods-form-control__root"
+          >
+            <div
+              class="ods-form-control__header"
+            >
+              <div
+                class="ods-form-control__label"
+              />
+            </div>
+            <div
+              class="ods-form-control__element"
+            >
+              <div
+                class="ods-input ods-input--disabled--text"
+              >
+                <input
+                  autocomplete="off"
+                  data-testid="datepicker-input-2"
+                  id="end-date"
+                  inputmode="numeric"
+                  maxlength="10"
+                  name="end-date"
+                  pattern="[0-9]*"
+                  placeholder="dd/mm/aaaa"
+                  readonly=""
+                  type="text"
+                  value="11/08/2022"
+                />
+                <div
+                  class="ods-input__adornment ods-input__adornment--placeholder"
+                >
+                  <svg
+                    fill="none"
+                    height="20"
+                    stroke="#B6B9CC"
+                    viewBox="0 0 24 24"
+                    width="20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `);
+});
+
+test('renders element with calendar open', () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '10/08/2022', to: '11/08/2022' }}
+      onSelect={onSelectMock}
+    />
+  );
+
+  fireEvent.click(screen.getByTestId('datepicker-input-1'));
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+});
+
+test('renders element with initial props', () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '10/08/2022', to: '11/08/2022' }}
+      onSelect={onSelectMock}
+      editable
+      error
+      helperText="error here!"
+    />
+  );
+
+  expect(screen.getByText('first-label')).toBeInTheDocument();
+  expect(screen.getByText('second-label')).toBeInTheDocument();
+  expect(screen.getAllByText('error here!')).toHaveLength(2);
+});
+
+test('renders element with calendar open and select dates values', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '', to: '' }}
+      onSelect={onSelectMock}
+    />
+  );
+
+  fireEvent.click(screen.getByTestId('datepicker-input-1'));
+
+  const datePicker = screen.getByTestId('datepicker-calendar');
+
+  expect(datePicker).toBeInTheDocument();
+
+  expect(screen.getByText('1')).toBeInTheDocument();
+
+  expect(screen.getByText('2')).toBeInTheDocument();
+
+  const firstDay = screen.getByText('1');
+  const secondDay = screen.getByText('2');
+
+  expect(firstDay).toBeInTheDocument();
+
+  expect(secondDay).toBeInTheDocument();
+
+  fireEvent.click(firstDay);
+  fireEvent.click(secondDay);
+
+  setTimeout(() => {
+    expect(datePicker).not.toBeInTheDocument();
+  }, 1000);
+});
+
+test('renders element with calendar open and click in start date grather than end date value', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{
+        from: '',
+        to: `${format(new Date(), ptBr?.formatLong?.date({ width: 'short' }))}`,
+      }}
+      onSelect={onSelectMock}
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  const tomorrow = new Date().getDate() + 1;
+
+  const fromDay = screen.getByText(tomorrow);
+
+  expect(fromDay).toBeInTheDocument();
+
+  fireEvent.click(fromDay);
+
+  expect(onSelectMock).toBeCalledWith({
+    from: `${format(
+      new Date().setDate(tomorrow),
+      ptBr?.formatLong?.date({ width: 'short' })
+    )}`,
+    to: '',
+  });
+});
+
+test('renders element with calendar open and click in start date with end date value', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{
+        from: '',
+        to: `${format(new Date(), ptBr?.formatLong?.date({ width: 'short' }))}`,
+      }}
+      onSelect={onSelectMock}
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  const yesterday = new Date().getDate() - 1;
+
+  const fromDay = screen.getByText(yesterday);
+
+  expect(fromDay).toBeInTheDocument();
+
+  fireEvent.click(fromDay);
+
+  expect(onSelectMock).toBeCalledWith({
+    from: `${format(
+      new Date().setDate(new Date().getDate() - 1),
+      ptBr?.formatLong?.date({ width: 'short' })
+    )}`,
+    to: `${format(new Date(), ptBr?.formatLong?.date({ width: 'short' }))}`,
+  });
+});
+
+test('renders element with calendar open and select dates values with hover', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '', to: '' }}
+      onSelect={onSelectMock}
+    />
+  );
+
+  fireEvent.click(screen.getByTestId('datepicker-input-1'));
+
+  const datePicker = screen.getByTestId('datepicker-calendar');
+
+  expect(datePicker).toBeInTheDocument();
+
+  expect(screen.getByText('1')).toBeInTheDocument();
+
+  expect(screen.getByText('2')).toBeInTheDocument();
+
+  const firstDay = screen.getByText('1');
+  const secondDay = screen.getByText('2');
+
+  expect(firstDay).toBeInTheDocument();
+
+  expect(secondDay).toBeInTheDocument();
+
+  fireEvent.click(firstDay);
+  fireEvent.mouseEnter(secondDay);
+
+  setTimeout(() => {
+    expect(datePicker).not.toBeInTheDocument();
+  }, 1000);
+});
+
+test('renders element with calendar open and select dates values with hover and save cache', async () => {
+  const onSelectMock = jest.fn();
+
+  const yesterday = new Date().getDate() - 1;
+
+  const today = new Date().getDate();
+
+  const tomorrow = new Date().getDate() + 1;
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{
+        from: `${format(
+          new Date().setDate(yesterday),
+          ptBr?.formatLong?.date({ width: 'short' })
+        )}`,
+        to: `${format(new Date(), ptBr?.formatLong?.date({ width: 'short' }))}`,
+      }}
+      onSelect={onSelectMock}
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  const input2 = screen.getByTestId('datepicker-input-2');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  const fromDay = screen.getByText(yesterday);
+
+  const toDay = screen.getByText(today);
+
+  const tomorrowDay = screen.getByText(tomorrow);
+
+  expect(fromDay).toBeInTheDocument();
+
+  expect(toDay).toBeInTheDocument();
+
+  fireEvent.click(fromDay);
+
+  fireEvent.click(toDay);
+
+  expect(onSelectMock).toBeCalledWith({
+    from: `${format(
+      new Date().setDate(yesterday),
+      ptBr?.formatLong?.date({ width: 'short' })
+    )}`,
+    to: `${format(new Date(), ptBr?.formatLong?.date({ width: 'short' }))}`,
+  });
+
+  fireEvent.click(input2);
+
+  fireEvent.mouseEnter(tomorrowDay);
+
+  const outside = screen.getByTestId('date-picker-outside');
+
+  fireEvent.click(outside);
+
+  expect(onSelectMock).toBeCalledWith({
+    from: `${format(
+      new Date().setDate(yesterday),
+      ptBr?.formatLong?.date({ width: 'short' })
+    )}`,
+    to: `${format(new Date(), ptBr?.formatLong?.date({ width: 'short' }))}`,
+  });
+});
+
+test('renders element with calendar open and select dates values with input', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '', to: '' }}
+      onSelect={onSelectMock}
+      editable
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+  const input2 = screen.getByTestId('datepicker-input-2');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  fireEvent.change(input1, { target: { value: '10/08/2022' } });
+  fireEvent.change(input2, { target: { value: '11/08/2022' } });
+
+  expect(input1).toHaveAttribute('value');
+  expect(input2).toHaveAttribute('value');
+});
+
+test('renders element with calendar open and english locale', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '', to: '' }}
+      onSelect={onSelectMock}
+      editable
+      locale={enUS}
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  expect(screen.getByText('Su')).toBeInTheDocument();
+
+  expect(screen.getByText('10')).toBeInTheDocument();
+
+  expect(screen.getAllByPlaceholderText('mm/dd/yyyy')).toHaveLength(2);
+});
+
+test('renders element with calendar open and today date', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '10/09/2022', to: '' }}
+      onSelect={onSelectMock}
+      startsToday
+      editable
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  const today = new Date().getDate();
+
+  const yesterday = new Date().getDate() - 1;
+
+  const fromDay = screen.getByText(today);
+  const beforeDay = screen.getByText(yesterday);
+
+  expect(fromDay).toBeInTheDocument();
+
+  expect(beforeDay.parentElement).toHaveClass('ods-datepicker__disabled');
+});
+
+test('renders element with calendar open and startsToday atribute on select yesterday', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '', to: '' }}
+      onSelect={onSelectMock}
+      startsToday
+      editable
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  const yesterday = new Date().getDate() - 1;
+
+  const fromDay = screen.getByText(yesterday);
+
+  expect(fromDay).toBeInTheDocument();
+
+  fireEvent.click(fromDay);
+
+  expect(onSelectMock).not.toBeCalledWith({
+    from: '',
+    to: '',
+  });
+});
+
+test('renders previous and next calendar button', () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '10/09/2022', to: '' }}
+      onSelect={onSelectMock}
+      startsToday
+      editable
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  const oldMonth = screen.getByTestId('calendar-caption-month');
+  const prevButton = screen.getByTestId('calendar-left-arrow');
+  const nextButton = screen.getByTestId('calendar-right-arrow');
+
+  expect(oldMonth).toBeInTheDocument();
+  expect(prevButton).toBeInTheDocument();
+  expect(nextButton).toBeInTheDocument();
+
+  fireEvent.click(nextButton);
+
+  const newMonth = screen.getByTestId('calendar-caption-month');
+
+  const t1 = oldMonth.textContent;
+  const t2 = newMonth.textContent;
+
+  fireEvent.click(prevButton);
+
+  expect(t1).toBe(t2);
+});
+
+test('renders correct date with correct mask value', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '10/08', to: '' }}
+      onSelect={onSelectMock}
+      editable
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  fireEvent.change(input1, { target: { value: '10' } });
+
+  expect(onSelectMock).toBeCalledWith({
+    from: '10',
+    to: '',
+  });
+
+  fireEvent.change(input1, { target: { value: '1008' } });
+
+  expect(onSelectMock).toBeCalledWith({
+    from: '10/08',
+    to: '',
+  });
+
+  fireEvent.change(input1, { target: { value: '10082' } });
+
+  expect(onSelectMock).toBeCalledWith({
+    from: '10/08/2',
+    to: '',
+  });
+});
+
+test('onSelect call with correct values', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '', to: '' }}
+      onSelect={onSelectMock}
+      editable
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  const today = new Date().getDate();
+
+  const fromDay = screen.getByText(today);
+
+  expect(fromDay).toBeInTheDocument();
+
+  fireEvent.click(fromDay);
+
+  expect(onSelectMock).toBeCalledWith({
+    from: `${format(new Date(), ptBr?.formatLong?.date({ width: 'short' }))}`,
+    to: '',
+  });
+});
+
+test('onSelect call with to date less than from date', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{
+        from: `${format(
+          new Date(),
+          ptBr?.formatLong?.date({ width: 'short' })
+        )}`,
+        to: '',
+      }}
+      onSelect={onSelectMock}
+      editable
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  fireEvent.click(input1);
+
+  expect(screen.getByTestId('datepicker-calendar')).toBeInTheDocument();
+
+  const today = new Date().getDate();
+
+  const yesterday = new Date().getDate() - 1;
+
+  const fromDay = screen.getByText(today);
+
+  const toDay = screen.getByText(yesterday);
+
+  expect(fromDay).toBeInTheDocument();
+
+  expect(toDay).toBeInTheDocument();
+
+  fireEvent.click(fromDay);
+
+  fireEvent.click(toDay);
+
+  expect(onSelectMock).toBeCalledWith({
+    from: `${format(
+      new Date().setDate(new Date().getDate() - 1),
+      ptBr?.formatLong?.date({ width: 'short' })
+    )}`,
+    to: '',
+  });
+});
+
+test('close calendar when click outside', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '10/09/2022', to: '' }}
+      onSelect={onSelectMock}
+      startsToday
+      editable
+    />
+  );
+
+  const input1 = screen.getByTestId('datepicker-input-1');
+
+  fireEvent.click(input1);
+
+  const calendar = screen.getByTestId('datepicker-calendar');
+
+  expect(calendar).toBeInTheDocument();
+
+  const outside = screen.getByTestId('date-picker-outside');
+
+  fireEvent.click(outside);
+
+  setTimeout(() => {
+    expect(calendar).not.toBeInTheDocument();
+  }, 1000);
+});
+
+test('renders input ref when click', async () => {
+  const onSelectMock = jest.fn();
+
+  render(
+    <DatePicker
+      labels={{ from: 'first-label', to: 'second-label' }}
+      values={{ from: '10/09/2022', to: '' }}
+      onSelect={onSelectMock}
+      startsToday
+      editable
+    />
+  );
+
+  const firstWrapper = screen.getByTestId('date-picker-first-field-wrapper');
+
+  expect(firstWrapper).toBeInTheDocument();
+
+  fireEvent.click(firstWrapper);
+
+  const secondWrapper = screen.getByTestId('date-picker-second-field-wrapper');
+
+  expect(secondWrapper).toBeInTheDocument();
+
+  fireEvent.click(secondWrapper);
+});
---
+++ packages/ocean-react/src/_util/useDatePicker.ts
@@ -0,0 +1,224 @@
+import React from 'react';
+import * as DateFns from 'date-fns';
+
+import * as DatePicker from '../DatePicker/DatePicker';
+
+import ptBr from 'date-fns/locale/pt-BR';
+
+import { DateRange, ClassNames, DateFormatter } from 'react-day-picker';
+
+type IDatePickerProps = Pick<
+  DatePicker.DatePickerProps,
+  'values' | 'onSelect' | 'startsToday' | 'locale'
+>;
+
+type IDatePickerReturn = {
+  input1Ref: React.Ref<HTMLInputElement>;
+  input2Ref: React.Ref<HTMLInputElement>;
+  showDayPicker: boolean;
+  fromDate: Date;
+  toDate: Date;
+  selectedDays: DateRange;
+  CustomStyles: ClassNames;
+  localeOption: DateFns.Locale;
+  currentField: string;
+  handleDayMouseEnter: (day: Date) => void;
+  handleDayClick: (day: Date) => void;
+  inputChange: ({ target }: React.ChangeEvent<HTMLInputElement>) => void;
+  createHandleToggleClick: (fieldId: string) => void;
+  disabledDays: (day: Date) => boolean;
+  formatDay: DateFormatter;
+  getInputPlaceholder: () => string;
+  handleCloseByOutside: () => void;
+};
+
+export default function useDatePicker({
+  values,
+  onSelect,
+  startsToday,
+  locale,
+}: IDatePickerProps): IDatePickerReturn {
+  const localeOption = locale || ptBr;
+  const localeDateFormat =
+    localeOption &&
+    localeOption.formatLong &&
+    localeOption.formatLong.date({ width: 'short' });
+
+  const input1Ref = React.useRef<HTMLInputElement>(null);
+  const input2Ref = React.useRef<HTMLInputElement>(null);
+
+  const [showDayPicker, setShowDayPicker] = React.useState(false);
+  const [currentField, setCurrentField] = React.useState<string>('');
+  const [datePickerCache, setDatePickerCache] =
+    React.useState<DatePicker.DatePickerFields>({ from: '', to: '' });
+
+  const fromDate = DateFns.parse(values.from, localeDateFormat, new Date());
+  const toDate = DateFns.parse(values.to, localeDateFormat, new Date());
+
+  React.useEffect(() => {
+    if (values.from === '') setCurrentField('');
+  }, [values.from]);
+
+  const updateState = (
+    updateData: DatePicker.DatePickerFields,
+    updateCache?: boolean
+  ) => {
+    onSelect(updateData);
+    updateCache && setDatePickerCache(updateData);
+  };
+
+  const handleValidateStartsToday = (day: Date) =>
+    Boolean(
+      startsToday &&
+        day < new Date(new Date().setDate(new Date().getDate() - 1))
+    );
+
+  const handleDayMouseEnter = (day: Date): void => {
+    const formattedDay = DateFns.format(day, localeDateFormat);
+
+    if (currentField === 'end-date' && !(values.from && day < fromDate)) {
+      setCurrentField('end-date');
+      updateState({ from: values.from, to: formattedDay });
+    }
+  };
+
+  const handleDayClick = (day: Date): void => {
+    if (!(startsToday && handleValidateStartsToday(day))) {
+      const formattedDay = DateFns.format(day, localeDateFormat);
+
+      if (currentField === 'start-date') {
+        if (day > toDate) {
+          updateState({ from: formattedDay, to: '' }, true);
+        } else {
+          updateState({ from: formattedDay, to: values.to });
+          setDatePickerCache({ from: formattedDay, to: values.to });
+        }
+        setCurrentField('end-date');
+      }
+
+      if (currentField === 'end-date') {
+        if (day < fromDate) {
+          updateState({ from: formattedDay, to: '' }, true);
+        } else {
+          updateState({ from: values.from, to: formattedDay }, true);
+          setShowDayPicker(false);
+          setCurrentField('');
+        }
+      }
+    }
+  };
+
+  const createHandleToggleClick = (fieldId: string) => {
+    setShowDayPicker(!showDayPicker);
+    setCurrentField(fieldId);
+  };
+
+  const disabledDays = (day: Date): boolean => {
+    const startToday = handleValidateStartsToday(day);
+
+    return startToday || (currentField === 'end-date' && day < fromDate);
+  };
+
+  const dateMask = (value: string) => {
+    const v = value.replace(/\D/g, '').slice(0, 10);
+
+    if (v.length >= 5) return `${v.slice(0, 2)}/${v.slice(2, 4)}/${v.slice(4)}`;
+    if (v.length >= 3) return `${v.slice(0, 2)}/${v.slice(2)}`;
+
+    return v;
+  };
+
+  const closeCalendarDelay = () => {
+    setTimeout(() => setShowDayPicker(false), 500);
+  };
+
+  const inputChange = ({
+    target,
+  }: React.ChangeEvent<HTMLInputElement>): void => {
+    const dataFormatted = dateMask(target.value);
+
+    if (target.id === 'start-date') {
+      setCurrentField('start-date');
+      updateState({ from: dataFormatted, to: '' }, true);
+
+      if (dataFormatted.length === localeDateFormat.length) {
+        setCurrentField('end-date');
+        if (input2Ref && input2Ref.current) input2Ref.current.focus();
+      }
+    } else {
+      setCurrentField('end-date');
+      updateState({ from: values.from, to: dataFormatted }, true);
+
+      if (dataFormatted.length === localeDateFormat.length) {
+        setCurrentField('');
+        closeCalendarDelay();
+      }
+    }
+  };
+
+  const handleCloseByOutside = () => {
+    if (showDayPicker) {
+      setShowDayPicker(false);
+      setCurrentField('');
+
+      if (datePickerCache.from !== '' && datePickerCache.to !== '')
+        updateState(datePickerCache);
+    }
+  };
+
+  const CustomStyles: ClassNames = {
+    root: `ods-datepicker__calendar ${
+      currentField === 'start-date'
+        ? 'ods-datepicker_calendar_m1'
+        : 'ods-datepicker_calendar_m2'
+    }`,
+    caption: 'ods-datepicker__caption',
+    nav_button: 'ods-datepicker__navButtons',
+    nav_button_previous: 'ods-datepicker__navButtonPrev',
+    nav_button_next: 'ods-datepicker__navButtonNext',
+    nav_icon: 'ods-datepicker__navIcon',
+    table: 'ods-datepicker__table',
+    head: 'ods-datepicker__head',
+    head_row: 'ods-datepicker__headRow',
+    tbody: 'ods-datepicker__body',
+    row: 'ods-datepicker__row',
+    cell: 'ods-datepicker__cell',
+    day: 'ods-datepicker__day',
+    day_today: 'ods-datepicker__today',
+    day_selected: 'ods-datepicker__selected',
+    day_disabled: 'ods-datepicker__disabled',
+    day_range_start: 'ods-datepicker__selectedStart',
+    day_range_end: 'ods-datepicker__selectedEnd',
+    day_range_middle: 'ods-datepicker__selectedMiddle',
+  };
+
+  const selectedDays: DateRange = {
+    from: fromDate,
+    to: toDate,
+  };
+
+  const formatDay: DateFormatter = (day) => DateFns.format(day, 'd');
+
+  const getInputPlaceholder = (): string =>
+    localeOption.code === 'pt-BR' ? 'dd/mm/aaaa' : 'mm/dd/yyyy';
+
+  return {
+    input1Ref,
+    input2Ref,
+    showDayPicker,
+    fromDate,
+    toDate,
+    selectedDays,
+    CustomStyles,
+    localeOption,
+    currentField,
+    handleDayMouseEnter,
+    handleDayClick,
+    inputChange,
+    createHandleToggleClick,
+    disabledDays,
+    formatDay,
+    getInputPlaceholder,
+    handleCloseByOutside,
+  };
+}
