c90f1941a5ecba672365b7864b5413c8ec99ed19
João Pedro Cordeiro <jpc5626@gmail.com>
2023-02-06 15:06:33 -0300

    feat: drawer component functional

--- packages/ocean-react/src/Drawer/Drawer.tsx
+++ packages/ocean-react/src/Drawer/Drawer.tsx
@@ -1,28 +1,49 @@
 import classNames from 'classnames';
 import React from 'react';
+import Button from '../Button/Button';
+
+interface DrawerProps {
+  children: React.ReactNode;
+  open: boolean;
+  onDrawerClose?(event: React.MouseEvent | React.KeyboardEvent): void;
+  backDropClose: () => void;
+  headerIcon: React.ReactNode;
+  iconAlignment?: string;
+}
 
 const Drawer = ({
   children,
   open,
   onDrawerClose,
   backDropClose,
-}): React.ReactElement => {
-  const a = '';
-
-  console.log('ded', open);
-
-  return (
-    <>
-      <div
-        className={classNames('backdrop', open && 'backdrop_open')}
-        aria-hidden="true"
-        onClick={backDropClose}
-      />
-      <div className={classNames('drawer', open && 'drawer_open')}>
-        {children}
-      </div>
-    </>
-  );
-};
+  headerIcon,
+  iconAlignment = 'right',
+}: DrawerProps): React.ReactElement => (
+  <>
+    <div
+      className={classNames(
+        'ods-drawer_backdrop',
+        open && 'ods-drawer_backdrop--open'
+      )}
+      aria-hidden="true"
+      onClick={backDropClose}
+    />
+    <div className={classNames('ods-drawer', open && 'ods-drawer--open')}>
+      {headerIcon && (
+        <div
+          className={classNames(
+            'ods-drawer_content--header',
+            `ods-drawer_content--header--${iconAlignment}`
+          )}
+        >
+          <Button type="button" onClick={onDrawerClose}>
+            {headerIcon}
+          </Button>
+        </div>
+      )}
+      {children}
+    </div>
+  </>
+);
 
 export default Drawer;
--- packages/ocean-react/src/Drawer/examples/SimpleDrawer.tsx
+++ packages/ocean-react/src/Drawer/examples/SimpleDrawer.tsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react';
-import Drawer from '../Drawer';
 
-const SimpleDrawer = ({ children }): React.ReactElement => {
+import { XOutline } from '@useblu/ocean-icons-react';
+import Drawer from '../Drawer';
+import Button from '../../Button';
+
+interface SimpleDrawerProps {
+  children: React.ReactElement;
+}
+
+const SimpleDrawer = ({ children }: SimpleDrawerProps): React.ReactElement => {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleOpen = () => setIsOpen((prevState) => !prevState);
@@ -10,13 +17,15 @@ const SimpleDrawer = ({ children }): React.ReactElement => {
 
   return (
     <>
-      <button onClick={toggleOpen} type="button">
-        open drawer!
-      </button>
+      <Button onClick={toggleOpen} type="button">
+        Open drawer
+      </Button>
       <Drawer
         open={isOpen}
         onDrawerClose={toggleOpen}
         backDropClose={toggleBackDropClose}
+        headerIcon={<XOutline />}
+        iconAlignment="left"
       >
         {children}
       </Drawer>
