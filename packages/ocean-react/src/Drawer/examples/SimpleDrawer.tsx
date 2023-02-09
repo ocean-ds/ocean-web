2659cf70effdd66b9e0993fd142980ece5047ab4
João Pedro Cordeiro <jpc5626@gmail.com>
2023-02-09 05:41:40 -0300

    feat: drawer component (#1061)
    * feat: initail drawer component commmit
    
    * feat: drawer component functional
    
    * feat: adjustments do drawer component children
    
    * fix: corrects styles issues
    
    * fix: drawer transition animation
    
    * fix: tries new transition
    
    * feat: creates unit tests
    
    * fix: correcting lint formatting
    
    * fix: corrects bug apponted by sonarCloud
    
    * fix: creates a default icon to the drawer component
    
    * fix: lint

---
+++ packages/ocean-react/src/Drawer/examples/SimpleDrawer.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+import Drawer from '../Drawer';
+import Button from '../../Button';
+
+export interface SimpleDrawerProps {
+  children: React.ReactElement;
+  open?: boolean;
+  iconAlignment?: string;
+}
+
+const SimpleDrawer = ({
+  children,
+  open,
+  iconAlignment,
+}: SimpleDrawerProps): React.ReactElement => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => setIsOpen((prevState) => !prevState);
+
+  const toggleOverlayClose = () => setIsOpen(false);
+
+  return (
+    <>
+      <Button onClick={toggleOpen} type="button">
+        Open drawer
+      </Button>
+      <Drawer
+        open={open || isOpen}
+        onDrawerClose={toggleOpen}
+        overlayClose={toggleOverlayClose}
+        iconAlignment={iconAlignment}
+      >
+        {children}
+      </Drawer>
+    </>
+  );
+};
+
+export default SimpleDrawer;
