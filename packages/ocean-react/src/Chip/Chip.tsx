2f671fd86b5660073e008f2ca284ed2a8b9518c1
Fabio_Oliveira <fabio.oliveira@useblu.com.br>
2023-03-14 16:21:47 -0300

    fix: adjust hover

--- packages/ocean-react/src/Chip/Chip.tsx
+++ packages/ocean-react/src/Chip/Chip.tsx
@@ -20,6 +20,7 @@ export type IChipProps = {
     } & React.SVGProps<SVGSVGElement> &
       React.RefAttributes<SVGSVGElement>
   >;
+  disabled?: boolean;
   onClick?: () => void;
 } & React.ComponentPropsWithoutRef<'div'>;
 
@@ -29,19 +30,27 @@ const Chip: React.FC<IChipProps> = ({
   isActive = false,
   counter = 0,
   iconLeft = null,
+  disabled = false,
   onClick,
 }) => (
   <div className={classNames('ods-chip', className)}>
     <Button
       variant="secondary"
-      className={classNames('ods-chip__button', isActive ? 'active' : '')}
+      className={classNames('ods-chip__button', isActive && 'active')}
       size="sm"
       onClick={onClick}
+      disabled={disabled}
     >
       {!!iconLeft && <span className="ods-chip__lefticon">{iconLeft}</span>}
       {label}
       {counter !== 0 && (
-        <Badge count={counter} className={!isActive ? 'ods-chip__badge' : ''} />
+        <Badge
+          count={counter}
+          className={classNames(
+            !isActive && 'ods-chip__badge',
+            disabled && 'ods-chip__badge-disabled'
+          )}
+        />
       )}
     </Button>
   </div>
