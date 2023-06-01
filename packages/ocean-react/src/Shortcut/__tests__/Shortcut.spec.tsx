518f8ee595125169a93fd1c4dab4192f4d69ece1
lpeil <luan@peil.dev>
2023-06-01 14:51:59 -0300

    test: finish specs

--- packages/ocean-react/src/Shortcut/__tests__/Shortcut.spec.tsx
+++ packages/ocean-react/src/Shortcut/__tests__/Shortcut.spec.tsx
@@ -3,12 +3,17 @@ import { render } from '@testing-library/react';
 
 import Shortcut, { ShortcutProps } from '../Shortcut';
 
+jest.mock('@useblu/ocean-icons-react', () => ({
+  LockClosed: () => 'mock-lock-closed',
+}));
+
 const setup = ({
   icon,
   label,
   description,
   size = 'medium',
   blocked = false,
+  disabled = false,
 }: ShortcutProps) => {
   render(
     <Shortcut
@@ -17,6 +22,7 @@ const setup = ({
       description={description}
       size={size}
       blocked={blocked}
+      disabled={disabled}
     />
   );
 };
@@ -29,15 +35,15 @@ test('renders shortcut component properly', () => {
       class="ods-shortcut ods-shortcut--medium"
     >
       <div
-        class="ods-shortcut__icon"
-      >
-        mock-home-xvg
-      </div>
-      <div
         class="ods-shortcut__content"
       >
+        <div
+          class="ods-shortcut__icon"
+        >
+          mock-home-xvg
+        </div>
         <h5
-          class="ods-shortcut__content__label ods-typography ods-typography__heading5"
+          class="ods-shortcut__label ods-typography ods-typography__heading5"
         >
           Label
         </h5>
@@ -54,24 +60,24 @@ test('renders shortcut component with description', () => {
       class="ods-shortcut ods-shortcut--medium"
     >
       <div
-        class="ods-shortcut__icon"
-      >
-        mock-home-xvg
-      </div>
-      <div
         class="ods-shortcut__content"
       >
+        <div
+          class="ods-shortcut__icon"
+        >
+          mock-home-xvg
+        </div>
         <h5
-          class="ods-shortcut__content__label ods-typography ods-typography__heading5"
+          class="ods-shortcut__label ods-typography ods-typography__heading5"
         >
           Label
         </h5>
-        <span
-          class="ods-shortcut__content__description ods-typography ods-typography__description"
-        >
-          Description
-        </span>
       </div>
+      <span
+        class="ods-shortcut__description ods-typography ods-typography__description"
+      >
+        Description
+      </span>
     </div>
   `);
 });
@@ -81,6 +87,23 @@ test('renders shortcut component with blocked state', () => {
 
   expect(document.querySelector('.ods-shortcut')).toHaveClass(
     'ods-shortcut--blocked'
+  );
+
+  expect(document.querySelector('.ods-shortcut__blocked'))
+    .toMatchInlineSnapshot(`
+      <div
+        class="ods-shortcut__blocked"
+      >
+        mock-lock-closed
+      </div>
+  `);
+});
+
+test('renders shortcut component with disabled state', () => {
+  setup({ icon: 'mock-home-xvg', label: 'Label', disabled: true });
+
+  expect(document.querySelector('.ods-shortcut')).toHaveClass(
+    'ods-shortcut--disabled'
   );
 });
 
