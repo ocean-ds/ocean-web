53537a392df99321240c31bd591db426a0dcaf4c
Daniela Muller <danielamuller20@gmail.com>
2024-06-21 17:31:07 -0300

    feat: update card list item component

--- packages/ocean-react/src/CardListItem/CardListItem.tsx
+++ packages/ocean-react/src/CardListItem/CardListItem.tsx
@@ -11,6 +11,7 @@ interface CardListItemProps {
   disabled?: boolean;
   fullWidth?: boolean;
   onClick?: () => void;
+  loading?: boolean;
 }
 
 const CardListItem = ({
@@ -23,37 +24,59 @@ const CardListItem = ({
   disabled = false,
   fullWidth = false,
   onClick,
-}: CardListItemProps): JSX.Element => (
-  <div
-    data-testid="card-list-item"
-    className={classNames(
-      'ods-card-list-item',
-      `ods-card-list-item--size-${size}`,
-      { 'ods-card-list-item--disabled': disabled },
-      { 'ods-card-list-item--full-width': fullWidth }
-    )}
-    onClick={() => {
-      if (!disabled && onClick) onClick();
-    }}
-  >
-    {leadingIcon && (
-      <div className="ods-card-list-item__leading-icon">{leadingIcon}</div>
-    )}
-    <div className="ods-card-list-item__content">
-      <div className="ods-card-list-item__content__title">{title}</div>
-      {description && (
-        <div className="ods-card-list-item__content__description">
-          {description}
-        </div>
+  loading,
+}: CardListItemProps): JSX.Element =>
+  loading ? (
+    <div
+      className={classNames(
+        'ods-card-list-item',
+        `ods-card-list-item--size-${size}`,
+        { 'ods-card-list-item--full-width': fullWidth },
+        {
+          'ods-card-list-item--loading': loading,
+        }
       )}
-      {caption && size === 'medium' && (
-        <div className="ods-card-list-item__content__caption">{caption}</div>
+    >
+      <div className="ods-card-list-item__circle" />
+      <div className="ods-card-list-item__lines">
+        <div className="ods-card-list-item__lines__line1" />
+        <div className="ods-card-list-item__lines__line2" />
+        {size === 'medium' && (
+          <div className="ods-card-list-item__lines__line3" />
+        )}
+      </div>
+    </div>
+  ) : (
+    <div
+      data-testid="card-list-item"
+      className={classNames(
+        'ods-card-list-item',
+        `ods-card-list-item--size-${size}`,
+        { 'ods-card-list-item--disabled': disabled },
+        { 'ods-card-list-item--full-width': fullWidth }
+      )}
+      onClick={() => {
+        if (!disabled && onClick) onClick();
+      }}
+    >
+      {leadingIcon && (
+        <div className="ods-card-list-item__leading-icon">{leadingIcon}</div>
+      )}
+      <div className="ods-card-list-item__content">
+        <div className="ods-card-list-item__content__title">{title}</div>
+        {description && (
+          <div className="ods-card-list-item__content__description">
+            {description}
+          </div>
+        )}
+        {caption && size === 'medium' && (
+          <div className="ods-card-list-item__content__caption">{caption}</div>
+        )}
+      </div>
+      {actionIcon && (
+        <div className="ods-card-list-item__action">{actionIcon}</div>
       )}
     </div>
-    {actionIcon && (
-      <div className="ods-card-list-item__action">{actionIcon}</div>
-    )}
-  </div>
-);
+  );
 
 export default CardListItem;
