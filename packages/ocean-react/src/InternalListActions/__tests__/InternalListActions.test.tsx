3f3399ada399d2c2909a0225b03bac84234d294e
GabrielFn <gabrielxfds@gmail.com>
2025-11-10 12:55:06 -0300

    fix: spec adjust

--- packages/ocean-react/src/InternalListActions/__tests__/InternalListActions.test.tsx
+++ packages/ocean-react/src/InternalListActions/__tests__/InternalListActions.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import InternalListActions from '../InternalListActions';
-import type { ActionItem } from '../InternalListActions';
+import type { ActionItem } from '../types';
 
 const mockActions: ActionItem[] = [
   {
