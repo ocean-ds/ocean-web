dafce530677eacd471f91af2d2a936042b14f2f5
Gabriel Fernandes da Silva <gabrielxfds@gmail.com>
2025-12-04 12:04:19 -0300

    feat: create internal list actions (#1194)

---
+++ packages/ocean-react/src/_shared/components/InternalListActions/index.ts
@@ -0,0 +1,3 @@
+export { default } from './InternalListActions';
+export { default as InternalListActions } from './InternalListActions';
+export type { ActionItem, InternalListActionsProps } from './types';
